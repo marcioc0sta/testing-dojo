a45eb3aecd64d3b00b005dd86460f4dfae10c92f
marcioc0sta <marciocst2@gmail.com>
2021-01-30 15:02:14 -0300

    fix: jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMApper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': '<rootDir>/.jest/identity-obj-proxy-esm.js',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
