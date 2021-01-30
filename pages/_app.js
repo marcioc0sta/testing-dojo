1da98f7117234fa4c52d840ef8956c3beaa91b01
marcioc0sta <marciocst2@gmail.com>
2021-01-30 14:27:33 -0300

    chore: remove next boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+function Tasky({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Tasky
